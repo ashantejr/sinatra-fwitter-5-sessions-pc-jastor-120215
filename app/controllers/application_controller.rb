9a595de0173a7bd17e59fcf764ccffed48e2ad1e
ashante <ashantejr@gmail.com>
2016-04-29 16:37:02 +0000

    yo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,11 +26,15 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweet' do
-    erb :tweet
+    if session[:user_id] #returns nil if there isn't one
+      erb :tweet
+    else
+      redirect "/login"
+    end
   end
 
   post '/tweet' do
-    user = User.find_by(:user_id => session[:user_id])
+    user = User.find_by(:id => session[:user_id])
     tweet = Tweet.new(:user => user, :status => params[:status])
     tweet.save
     redirect '/'
@@ -39,6 +43,10 @@ class ApplicationController < Sinatra::Base
   get '/users' do
     @users = User.all
     erb :users
+  end
+  
+  get '/signup' do
+    erb :signup
   end
 
   post '/sign-up' do
@@ -50,13 +58,8 @@ class ApplicationController < Sinatra::Base
   get '/login' do
     erb :login
   end
-
+ 
   post '/login' do
-     puts params
-    'hello login request'
-  end
-
-   post '/login' do
      user = User.find_by(:username => params[:username])
      if user
         session[:user_id] = user.id
@@ -67,11 +70,19 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweet' do
+    erb :tweet
     if session[:user_id] #returns nil if there isn't one
       erb :tweet
     else
       redirect "/login"
     end
+  end
+  
+  post '/tweet' do
+    user = User.find_by(:id => session[:user_id])
+    tweet = Tweet.new(:user => user, :status => params[:status])
+    tweet.save
+    redirect '/'
   end
 
   get '/logout' do
@@ -79,4 +90,18 @@ class ApplicationController < Sinatra::Base
     redirect '/login'
   end
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+ 
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+ 
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
